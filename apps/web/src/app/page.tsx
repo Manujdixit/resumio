468811cbac720b2598ccbab04d454e1f72f4b330
codeRED24 <Himanshu03213040@gmail.com>
2025-11-22 17:54:09 +0530

    landing

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Features } from "@/components/components/Features";
-import { Footer } from "@/components/components/Footer";
-import { Hero } from "@/components/components/Hero";
-import { HowItWorks } from "@/components/components/HowItWorks";
-import { Navbar } from "@/components/components/Navbar";
-import { Pricing } from "@/components/components/Pricing";
+import { Features } from "@/components/landing/Features";
+import { Footer } from "@/components/landing/Footer";
+import { Hero } from "@/components/landing/Hero";
+import { HowItWorks } from "@/components/landing/HowItWorks";
+import { Navbar } from "@/components/landing/Navbar";
+import { Pricing } from "@/components/landing/Pricing";
 
 const App: React.FC = () => {
   return (
